54bbbdb500a97667793a7625f2d8ccb4880728b5
Sailesh-Patel <150167583+Sailesh-Patel@users.noreply.github.com>
2024-05-07 14:11:12 +0100

    saving branch initial push

--- src/Routing/SavingsCalculator.jsx
+++ src/Routing/SavingsCalculator.jsx
@@ -1,6 +1,31 @@
+import "./SavingsCalculator.css";
+
 function SavingsCalculator() {
     return ( 
-        <h1>Savings Calculator</h1>
+        <div>
+            <h1>Savings Calculator</h1>
+            <container className="container_calculator">
+                <label>Savings Name:</label>
+                <input type="text"/>
+
+                <label >Savings Goal (£):</label>
+                <input type="double" min={0}/>
+
+                <label >Save by? (date):</label>
+                <input type="text"  />
+
+                <label >Frequency:</label>
+                <input type="text" />
+
+                <button className="submit">Submit</button>
+            </container>
+
+            <container >
+
+            </container>
+        </div>
+       
+
      );
 }
 
